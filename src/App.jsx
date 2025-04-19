f0ac2561e5f3f224417f806423305f455c9744e7
DoriaFabio <doriafabio2001@gmail.com>
2025-04-19 11:48:12 +0200

    add tailwind and do milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@
 import './App.css'
 
 function App() {
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
 
   return (
     <>
-      
+      <h1 className='text-3xl m-5'>Carrello della spesa</h1>
+      <ul className='list-disc m-10'>
+        {products.map((p, i) => (
+          <li key={i}>
+            Nome: {p.name} - Prezzo: {p.price} €
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
